1456788221ec5aee95e027f9c0951a30f35788a3
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-13 11:58:15 -0500

    Check if num is even or odd

---
+++ check_even.py
@@ -0,0 +1,13 @@
+###############################################################################
+# Name: Jason D. F. D'Oyley
+# Date: November 13, 2022
+# Task 2 - Write the Program
+# Description: Write a program to only print out even numbers.
+###############################################################################
+print("Student ID#: JASDOY5736")
+num = int(input("Enter a num: "))
+
+if (num % 2):
+    print("Number is Odd")
+else:
+    print("Number is Even")
